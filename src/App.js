d5d6048c01faf08d4db144121236a864da0a5db3
Suyash Srivastava <suyash.1rbl@gmail.com>
2024-07-21 21:35:58 +0530

    First test commit in shisu food app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shisu A Food App...
         </a>
       </header>
     </div>
